02f425e1349ea45f540fb80a250f7afd8951c01c
jerrelllzw <lowjerrell47@gmail.com>
2025-05-31 16:07:06 +0800

    remove unused file

--- src/utils/openMaps.ts
+++
@@ -1,6 +0,0 @@
-import { Linking } from 'react-native';
-
-export const openInMaps = (place:any) => {
-  const url = `https://www.google.com/maps/search/?api=1&query=${place.lat},${place.lng}`;
-  Linking.openURL(url);
-};
