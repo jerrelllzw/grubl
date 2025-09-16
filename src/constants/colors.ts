a7a22ae04dea3defe3c2ec5f31535747c932a9ec
Jerrell Low <lowjerrell47@gmail.com>
2025-09-16 10:02:49 +0800

    Delete colors.ts

--- src/constants/colors.ts
+++
@@ -1,6 +0,0 @@
-export const colors = {
-  primary: '#FE7743',    // Bright orange — buttons, highlights
-  secondary: '#EFEEEA',  // Light gray-beige — backgrounds or secondary UI
-  tertiary: '#273F4F',   // Dark bluish-gray — text or dark UI elements
-  accent: '#000000',     // Black — contrast or emphasis
-};
